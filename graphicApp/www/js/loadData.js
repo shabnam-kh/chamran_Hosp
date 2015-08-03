fea5dc237eca9e2e3f4a8a52235c5604e2af6578
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-08-03 15:57:07 +0430

    adapt element to server data

--- graphicApp/www/js/loadData.js
+++ graphicApp/www/js/loadData.js
@@ -2,12 +2,19 @@
  * Created by klappo on 8/1/15.
  */
 sendReq(function(){},function(data){
-   updator(data);
+   updator(JSON.parse(data));
 });
-var yourNumber = parseInt("000f", 16);
-var chamranData={"Digital":{"124": "0","125":"1","126":"0"}};
-console.log("json data "+JSON.stringify(chamranData["Digital"]["125"]))
-console.log("convert num is "+yourNumber);
-function updator(data){
 
+
+function updator(data){
+    $.each(data.Digital,function(key,val){
+        console.log("key "+key);
+        console.log("value "+val);
+        if(val==1){
+            $("#"+key).show();
+        }
+        if(key==400 || key==407){
+            $(".id"+key).show();
+        }
+    })
 }
