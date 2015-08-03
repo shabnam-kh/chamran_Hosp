e057426ae32f7234b4262a80fb5b48aa17e5228b
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-08-03 16:21:17 +0430

    show temp

--- graphicApp/www/js/loadData.js
+++ graphicApp/www/js/loadData.js
@@ -5,13 +5,15 @@ sendReq(function(){},function(data){
    updator(JSON.parse(data));
 });
 
-
 function updator(data){
     $.each(data.Digital,function(key,val){
         console.log("key "+key);
         console.log("value "+val);
         if(val==1){
             $("#"+key).show();
+        }
+        else{
+            $("#"+key).text(val);
         }
         if(key==400 || key==407){
             $(".id"+key).show();
