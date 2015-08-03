d403a5e962a238d1ec9b6e63a568358085e246de
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-08-03 12:31:55 +0430

    add water level sign

--- graphicApp/www/js/loadData.js
+++ graphicApp/www/js/loadData.js
@@ -1,9 +1,13 @@
 /**
  * Created by klappo on 8/1/15.
  */
-sendReq(function(){},function(){});
+sendReq(function(){},function(data){
+   updator(data);
+});
 var yourNumber = parseInt("000f", 16);
+var chamranData={"Digital":{"124": "0","125":"1","126":"0"}};
+console.log("json data "+JSON.stringify(chamranData["Digital"]["125"]))
 console.log("convert num is "+yourNumber);
-function convertor(data){
+function updator(data){
 
 }
